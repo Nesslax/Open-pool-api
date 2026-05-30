95e16a2fb80be16cd9b3cd815cc154917fb7f912
Youssef Diouri <youssefdiouri4@gmail.com>
2026-05-30 15:00:51 +0800

    fix(metrics): add bearer token auth guard to /metrics endpoint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const { metricsMiddleware, metricsHandler } = require('./src/metrics');
+const { metricsMiddleware, metricsAuthMiddleware, metricsHandler } = require('./src/metrics');
 const app = express()
 //const {setupLogging} = require("./logging");
 const port = 3030
 
 app.use(metricsMiddleware);
-app.get('/metrics', metricsHandler);
+app.get('/metrics', metricsAuthMiddleware, metricsHandler);
 
 
 const nanopoolRoutes = require("./pools/nanopool/routes.js");
